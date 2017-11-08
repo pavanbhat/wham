0a0c54fc486d70e71214b391bae1e3bc64c9070b
Pavan <pxb8715@rit.edu>
2017-11-08 18:08:41 -0500

    First commit!

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Abhishek's first React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
